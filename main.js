a96e9570f3190f7c49c715a7b4512d56fe59ecca
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2023-07-09 13:02:07 -0400

    api callsJLL

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=boolean"
+
+document.getElementById("getQuestionsButton").addEventListener("click",function(event) 
+    {
+     event.preventDefault();
+
+
+
+  
+   
+    })
